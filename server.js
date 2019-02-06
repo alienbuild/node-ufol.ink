db6b74b7f97659f57f9f481cfd0280643b2f6781
Liam <lk@alienbuild.uk>
2019-02-06 10:30:43 +0000

    + Node redirect handler

--- server.js
+++ server.js
@@ -30,10 +30,21 @@ app.use('/api/redirect', redirect);
 
 // Serve static assets if in productions
 if (process.env.NODE_ENV === 'production' ) {
+    // Redirect
+    app.get('/:hash', function(req, res) {
+        const id = req.params.hash;
+        URL.findOne({ _id: id }, function(err, doc) {
+            if(doc) {
+                res.redirect(doc.url);
+            } else {
+                res.redirect('/');
+            }
+        });
+    });
     // Set static folder
     app.use(express.static('client/build'));
     // Load static file
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
